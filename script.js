27b2a6d2b7560e037b32c83c4b10342c410d93e1
marcladefoged <marcladefogedjensen@gmail.com>
2024-03-04 20:07:30 +0100

    light/dark mode added

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ hamburgerBtn.addEventListener('click', sidebarToggle);
 function sidebarToggle () {
     sideBar.classList.toggle('active');
 }
+
+// Code for Light/Dark Mode Toggle
+let modeSwitcher = document.querySelector('.mode-switch i');
+let body = document.querySelector('body');
+
+modeSwitcher.addEventListener('click', modeSwitch);
+
+function modeSwitch() {
+    body.classList.toggle('active');
+}
